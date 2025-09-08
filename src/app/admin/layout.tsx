b265faf39048c69f14ba41b4735df859f27a15ad
Manish Sonawane <manishsonawane3010@gmail.com>
2025-09-08 10:06:04 +0000

    i still am seeing admin sidenar in admin login page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,23 +1,8 @@
-import AdminSidebar from "@/components/admin-sidebar";
-import AdminHeader from "@/components/admin-header";
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        <AdminSidebar />
-        <SidebarInset className="bg-muted/40">
-          <AdminHeader />
-          <main className="grid flex-1 items-start gap-8 p-4 sm:px-6">
-            {children}
-          </main>
-        </SidebarInset>
-      </div>
-    </SidebarProvider>
-  );
-}
+export default function AdminRootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return <>{children}</>;
+  }
