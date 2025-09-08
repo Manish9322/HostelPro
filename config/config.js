033aab2ffde107c8dfff9fd4f74a688b14c0b9d2
Manish Sonawane <manishsonawane3010@gmail.com>
2025-09-08 12:07:17 +0000

    mongodb+srv://manish3010:Manish%40123@cluster0.dyifq.mongodb.net/hostelp

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
 // In a real application, you should use environment variables for sensitive data.
-// For this example, we'll hardcode the value.
-// You can create a .env.local file and add your MONGODB_URI there.
+// You can create a .env file and add your MONGODB_URI there.
 // Example: MONGODB_URI=mongodb+srv://<user>:<password>@<cluster-url>/<database-name>?retryWrites=true&w=majority
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/hostelpro";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export { MONGODB_URI };
